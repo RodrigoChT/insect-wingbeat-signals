6863bba988e90e90def0fb48c139d2f016f06871
rodrigo <rodrigo.chr@gmail.com>
2020-02-03 20:28:11 +0100

    Missed file for previous commit

---
+++ wavs.py
@@ -0,0 +1,32 @@
+import scipy.signal as sig
+import pandas as pd
+
+from freqs import Freqs
+
+class Wavs:
+    def __init__(self, raw, frequency, amount):
+        self.wav_raw = raw
+        self.frequency = frequency
+        self.amount = amount
+
+    def filter_data(self,
+                    lowcut,
+                    highcut,
+                    order):
+        """Apply a 4th order Butterworth filter"""
+        nyq = 0.5 * self.frequency
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = sig.butter(order, [low, high], btype='band')
+        self.wav_filtered = [sig.lfilter(b, a, wav)
+                         for wav in self.wav_raw]
+
+    def perform_fourier_transform(self):
+        """Performn a Fourier Transform"""
+        freq_data = [sig.welch(wav, self.frequency)
+                     for wav in self.wav_filtered]
+        freq_data = Freqs([pd.DataFrame(data = dict(freq = obs[0],
+                                                    power = obs[1]))
+                           for obs in freq_data],
+                          self.amount)
+        return freq_data
